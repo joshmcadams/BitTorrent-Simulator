6f83a9fa2b5f827443641531d1c2a4cde6ac30c9
Josh McAdams <joshua.mcadams@gmail.com>
2010-04-13 22:18:31 -0500

    eclipse formatted... lamely commented

--- java/edu/ualr/bittorrent/PeerMessage.java
+++ java/edu/ualr/bittorrent/PeerMessage.java
@@ -6,19 +6,43 @@ import edu.ualr.bittorrent.interfaces.HasPeer;
 import edu.ualr.bittorrent.interfaces.Message;
 import edu.ualr.bittorrent.interfaces.Peer;
 
+/**
+ * BitTorrent peers exchange messages. Most are documented, some are
+ * experimental and simply ignored by standard clients. Regardless, all should
+ * inherit from this class if they can. If not, they should implement the
+ * {@link Message} and {@link HasPeer} interface so that they are compatible
+ * with this simulator.
+ *
+ * @param <T>
+ *          The type of message.
+ */
 public class PeerMessage<T> implements HasPeer, Message {
   private final Peer peer;
   private final Type type;
 
+  /**
+   * Create a new {@link PeerMessage}.
+   *
+   * @param peer
+   *          The {@link Peer} sending the message.
+   * @param type
+   *          The {@link Type} of message being sent.
+   */
   public PeerMessage(Peer peer, Type type) {
     this.peer = Preconditions.checkNotNull(peer);
     this.type = Preconditions.checkNotNull(type);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public Peer getPeer() {
     return peer;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public Type getType() {
     return type;
   }
--- java/edu/ualr/bittorrent/Simulator.java
+++ java/edu/ualr/bittorrent/Simulator.java
@@ -24,29 +24,66 @@ import edu.ualr.bittorrent.interfaces.Peer;
 import edu.ualr.bittorrent.interfaces.PeerProvider;
 import edu.ualr.bittorrent.interfaces.Tracker;
 
+/**
+ * Driver class that defines the experiment. The {@link Simulator} binds all
+ * components of a swarm together. This includes the {@link PeerProvider} that
+ * provides the peers to the simulated swarm and the {@link Metainfo} that the
+ * swarm is using to calculate its downloads.
+ *
+ * @author jmcadams
+ */
 public class Simulator {
   private final PeerProvider peerProvider;
   private final Metainfo metainfo;
   private final List<Peer> peers = Lists.newArrayList();
-  private final static Logger logger = Logger.getLogger(Simulator.class);
   private final ExecutorService executor;
 
-  public Simulator(PeerProvider peerProvider, Metainfo metainfo, Integer threadCount) {
+  private final static Logger logger = Logger.getLogger(Simulator.class);
+
+  /**
+   * Create a new {@link Simulator}.
+   *
+   * @param peerProvider
+   *          class to provide {@link Peer}s to the experiment.
+   * @param metainfo
+   *          metadata information about the file(s) to be downloaded in the
+   *          experiment.
+   * @param threadCount
+   *          number of threads that the experiment should use to execute.
+   */
+  public Simulator(PeerProvider peerProvider, Metainfo metainfo,
+      Integer threadCount) {
     this.peerProvider = Preconditions.checkNotNull(peerProvider);
     this.metainfo = Preconditions.checkNotNull(metainfo);
-    this.executor = Executors.newFixedThreadPool(Preconditions.checkNotNull(threadCount));
+    this.executor = Executors.newFixedThreadPool(Preconditions
+        .checkNotNull(threadCount));
   }
 
+  /**
+   * Private subclass used to terminate the experiment after a fixed period of
+   * time.
+   */
   private class ExperimentTerminator implements Runnable {
     private final Long millisecondsToRun;
 
+    /**
+     * Create a new thread that will terminate the experiment after a provided
+     * period of time.
+     *
+     * @param millisecondsToRun
+     */
     public ExperimentTerminator(Long millisecondsToRun) {
       this.millisecondsToRun = Preconditions.checkNotNull(millisecondsToRun);
     }
 
+    /**
+     * Run a thread, waiting on a timeout that is used to shutdown the
+     * experiment.
+     */
     public void run() {
       try {
-        logger.info(String.format("Experiment limited to %d microseconds", millisecondsToRun));
+        logger.info(String.format("Experiment limited to %d microseconds",
+            millisecondsToRun));
         executor.awaitTermination(millisecondsToRun, TimeUnit.MILLISECONDS);
         if (!executor.isShutdown()) {
           logger.info("Stopping experiment before all threads are complete");
@@ -58,6 +95,11 @@ public class Simulator {
     }
   }
 
+  /**
+   * Start a given experiment for a limited amount of time.
+   *
+   * @param millisecondsToRun
+   */
   public void runExperiment(Long millisecondsToRun) {
     logger.info("Experiment starting");
 
@@ -71,12 +113,21 @@ public class Simulator {
     }
   }
 
+  /**
+   * Set the max time for the experiment.
+   *
+   * @param millisecondsToRun
+   */
   private void setTimeout(Long millisecondsToRun) {
     if (millisecondsToRun != null) {
       executor.execute(new ExperimentTerminator(millisecondsToRun));
     }
   }
 
+  /**
+   * Torrents can have one or more {@link Tracker}s. In our experiments, each of
+   * these {@link Tracker}s will have its own thread.
+   */
   private void spawnTrackers() {
     for (Tracker tracker : metainfo.getTrackers()) {
       if (executor.isShutdown()) {
@@ -86,6 +137,13 @@ public class Simulator {
     }
   }
 
+  /**
+   * The {@link PeerProvider} is called upon throughout the experiment to
+   * provide new peers to the swarm. This allows for an experiment to more
+   * closely relate to a traditional swarm where all of the peers join over a
+   * period of time. This method repeatedly calls upon the {@link PeerProvider}
+   * to provide new {@link Peers},starting each in a new thread.
+   */
   private void spawnPeers() {
     ImmutableList<Peer> newPeers;
     while ((newPeers = peerProvider.addPeers()) != null) {
@@ -98,12 +156,19 @@ public class Simulator {
           logger.info("Executor is shutdown");
           return;
         }
-        logger.info(String.format("Requesting execution of peer %s", new String(peer.getId())));
+        logger.info(String.format("Requesting execution of peer %s",
+            new String(peer.getId())));
         executor.execute(peer);
       }
     }
   }
 
+  /**
+   * Run an experiment.
+   *
+   * @param args
+   * @throws NoSuchAlgorithmException
+   */
   public static void main(String[] args) throws NoSuchAlgorithmException {
     BasicConfigurator.configure();
 
@@ -123,18 +188,18 @@ public class Simulator {
       }
       String dataPiece = stringBuilder.toString();
       data.put(i, dataPiece.getBytes());
-      pieces.add(new String(MessageDigest.getInstance("SHA").digest(dataPiece.getBytes())));
+      pieces.add(new String(MessageDigest.getInstance("SHA").digest(
+          dataPiece.getBytes())));
     }
 
-   Metainfo.File file = new MetainfoImpl.FileImpl(
-       new Long(pieceLength * 10), ImmutableList.of("x.txt"));
-   ImmutableList<Metainfo.File> files = ImmutableList.of(file);
+    Metainfo.File file = new MetainfoImpl.FileImpl(new Long(pieceLength * 10),
+        ImmutableList.of("x.txt"));
+    ImmutableList<Metainfo.File> files = ImmutableList.of(file);
 
-   Metainfo metainfo = new MetainfoImpl(trackers, ImmutableList.copyOf(pieces), pieceLength, files);
+    Metainfo metainfo = new MetainfoImpl(trackers,
+        ImmutableList.copyOf(pieces), pieceLength, files);
 
-   new Simulator(
-        new PeerProviderImpl(metainfo, data),
-        metainfo, 100
-        ).runExperiment(null);
+    new Simulator(new PeerProviderImpl(metainfo, data), metainfo, 100)
+        .runExperiment(null);
   }
 }
