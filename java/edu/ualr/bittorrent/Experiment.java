2e6eb32c2d5a9681f992a0bd8eb1b59e8893323c
Josh McAdams <joshua.mcadams@gmail.com>
2010-04-18 15:16:34 -0500

    removing unthrown exception declaration

--- java/edu/ualr/bittorrent/Experiment.java
+++ java/edu/ualr/bittorrent/Experiment.java
@@ -1,7 +1,5 @@
 package edu.ualr.bittorrent;
 
-import java.security.NoSuchAlgorithmException;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
@@ -16,9 +14,8 @@ public class Experiment {
    * Run an experiment.
    *
    * @param args
-   * @throws NoSuchAlgorithmException
    */
-  public static void main(String[] args) throws NoSuchAlgorithmException {
+  public static void main(String[] args) {
     Injector injector = Guice.createInjector(new ExperimentModule());
 
     SimulatorImpl simulator = injector.getInstance(SimulatorImpl.class);
